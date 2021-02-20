a6c4dc997c75c072bc544fc9790638e8913105f7
Nishad Sandilya <67221201+NishadSandilya@users.noreply.github.com>
2021-02-20 20:26:25 +0530

    Added Features v1.2.3

--- js/form.js
+++ js/form.js
@@ -1,4 +1,19 @@
-document.querySelector('.container span').addEventListener('click', ()=>{
+
+window.onload = ()=>{
+    if(window.outerHeight < window.outerWidth){
+        document.querySelector('.unsupported-res').classList.remove('make-invisible');
+        document.querySelector('.unsupported-res p').innerHTML = "Mindium was initially developed for mobile devices alone. We are currently working on the desktop version. For now, please switch to your mobile to view mindium. Thank You";
+    }
+}
+window.addEventListener("orientationchange", function(event) {
+    if(event.target.screen.width > event.target.screen.height){
+        document.querySelector('.unsupported-res').classList.remove('make-invisible');
+        document.querySelector('.unsupported-res p').innerHTML = "Mindium looks best in portrait mode. Please switch to portrait mode on your device. Thank You";
+    }
+    else{
+        document.querySelector('.unsupported-res').classList.add('make-invisible');
+    }
+});document.querySelector('.container span').addEventListener('click', ()=>{
     window.open('','_self').close();
 })
 function generateFragments(){
--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 window.onload = ()=>{
     if(window.outerHeight < window.outerWidth){
-        document.querySelector('body').classList.add('make-invisible');
-        alert('mindium. is developed to be used only on mobile phones for now. Please switch to your phone to view mindium. If you are on a phone and you are getting this, please switch to portrait mode. Thank you');
+        document.querySelector('.unsupported-res').classList.remove('make-invisible');
+        document.querySelector('.unsupported-res p').innerHTML = "Mindium was initially developed for mobile devices alone. We are currently working on the desktop version. For now, please switch to your mobile to view mindium. Thank You";
     }
 }
 window.addEventListener("orientationchange", function(event) {
     if(event.target.screen.width > event.target.screen.height){
-        document.querySelector('body').classList.add('make-invisible');
-        alert('mindium. is developed to be used only in portrait mode. Please switch to portrait to view mindium. Thank you');
+        document.querySelector('.unsupported-res').classList.remove('make-invisible');
+        document.querySelector('.unsupported-res p').innerHTML = "Mindium looks best in portrait mode. Please switch to portrait mode on your device. Thank You";
     }
     else{
-        document.querySelector('body').classList.remove('make-invisible');
+        document.querySelector('.unsupported-res').classList.add('make-invisible');
     }
 });
 const mindium = document.querySelector('.mindium');
